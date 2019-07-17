cc44abc5ea308a2b4165cf40bae51db44fefccb5
convlCode <q838540783@163.com>
2019-07-17 21:22:25 +0800

    Draw a rotating 3D box!

--- GLWidget.cpp
+++ GLWidget.cpp
@@ -22,38 +22,63 @@ void GLWidget::initializeGL()
     core = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_3_Core>();
     myShader = new Shader(":/shaders/vertexShaderSource.vs",":/shaders/fragShaderSource.fs");
 
-    GLfloat vertices[] = {
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
-    };
+    float vertices[] = {
+           -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+           -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-    GLuint indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+           -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+           -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+           -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+           -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+           -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+           -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+           -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+           -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+           -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+           -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+           -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+           -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     core->glGenVertexArrays(1, &VAO);
     core->glGenBuffers(1, &VBO);
-    core->glGenBuffers(1,&EBO);
 
     core->glBindVertexArray(VAO);
 
     core->glBindBuffer(GL_ARRAY_BUFFER, VBO);
     core->glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    core->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-    core->glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
-    core->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<void*>(0));
+    core->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<void*>(0));
     core->glEnableVertexAttribArray(0);
 
-    core->glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8 * sizeof(GLfloat),reinterpret_cast<void*>(3*sizeof(GLfloat)));
+    core->glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),reinterpret_cast<void*>(3*sizeof(GLfloat)));
     core->glEnableVertexAttribArray(1);
-
-    core->glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,8*sizeof(GLfloat),reinterpret_cast<void*>(6*sizeof(GLfloat)));
-    core->glEnableVertexAttribArray(2);
 
     texture1 = new QOpenGLTexture(QOpenGLTexture::Target2D);
     texture1->setFormat(QOpenGLTexture::RGBFormat);
@@ -81,19 +106,22 @@ void GLWidget::initializeGL()
     texture2->setMagnificationFilter(QOpenGLTexture::Linear);
 
     myShader->use();
-    myShader->shaderProgram.setUniformValue(myShader->shaderProgram.uniformLocation("texture1"),0);
-    myShader->shaderProgram.setUniformValue(myShader->shaderProgram.uniformLocation("texture2"),1);
+    myShader->setInt("texture1",0);
+    myShader->setInt("texture2",1);
 
-    //time.start();
-    QMatrix4x4 model,view,projection;
-    model.rotate(-55.0f,QVector3D(1.0f,0.0f,0.0f));
+    time.start();
+
+    QMatrix4x4 view,projection;
+    //model.rotate(-55.0f,QVector3D(1.0f,0.0f,0.0f));
     view.translate(QVector3D(0.0f,0.0f,-3.0f));
     projection.perspective(45.0f,static_cast<float>(width())/static_cast<float>(height()),0.1f,100.0f);
 
     myShader->use();
-    myShader->setMat4("model",model);
+    //myShader->setMat4("model",model);
     myShader->setMat4("view",view);
     myShader->setMat4("projection",projection);
+
+    core->glEnable(GL_DEPTH_TEST);
 }
 
 void GLWidget::resizeGL(int w, int h)
@@ -104,17 +132,20 @@ void GLWidget::resizeGL(int w, int h)
 void GLWidget::paintGL()
 {
     core->glClearColor(0.2f,0.3f,0.3f,1.0f);
-    core->glClear(GL_COLOR_BUFFER_BIT);
+    core->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     core->glActiveTexture(GL_TEXTURE0);
     texture1->bind();
     core->glActiveTexture(GL_TEXTURE1);
     texture2->bind();
 
+    QMatrix4x4 model;
+    model.rotate(static_cast<float>(time.elapsed())/10.0f,QVector3D(0.5f,1.0f,0.0f));
+
     myShader->use();
+    myShader->setMat4("model",model);
     core->glBindVertexArray(VAO);
-    //core->glDrawArrays(GL_TRIANGLES, 0, 3);
-    //core->glUseProgram(0);
-    core->glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,reinterpret_cast<GLvoid*>(0));
+    core->glDrawArrays(GL_TRIANGLES, 0, 36);
+
     update();
 }
--- Shader.h
+++ Shader.h
@@ -15,6 +15,20 @@ public:
     void use(){
         shaderProgram.bind();
     }
+    inline void setMat4(const QString& name,const QMatrix4x4& value);
+    inline void setInt(const QString& name,const GLint value);
 };
+
+inline void Shader::setMat4(const QString &name, const QMatrix4x4 &value)
+{
+    GLint loc = shaderProgram.uniformLocation(name);
+    shaderProgram.setUniformValue(loc,value);
+}
+
+inline void Shader::setInt(const QString &name, const GLint value)
+{
+    GLint loc = shaderProgram.uniformLocation(name);
+    shaderProgram.setUniformValue(loc,value);
+}
 
 #endif // SHADER_H
